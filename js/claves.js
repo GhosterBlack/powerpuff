be39a5aadc79b34dc7f0e9ec87731e0c4276a76a
GhosterBlack <117308243+GhosterBlack@users.noreply.github.com>
2023-08-22 09:40:21 -0500

    Update claves.js

--- js/claves.js
+++ js/claves.js
@@ -27,7 +27,7 @@ const claves = {
     },
     franker: {
         fecha: "08-22-2023",
-        activate: false,
+        activate: true,
         admin: true,
     },
 
@@ -48,13 +48,13 @@ const claves = {
     
    A261211f1gi: {
     fecha: "09-22-2023",
-    activate: true,
+    activate: false,
     admin: false,
     },
 
     A3m24332348: {
     fecha: "09-22-2023",
-    activate: true,
+    activate: false,
     admin: false,
     },
 
