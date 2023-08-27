1edd038d23c8ffbd811f57404626688a5820b5ed
GhosterBlack <117308243+GhosterBlack@users.noreply.github.com>
2023-08-27 06:24:00 -0500

    Update claves.js

--- js/claves.js
+++ js/claves.js
@@ -39,38 +39,110 @@ const claves = {
     },
     
    A261211f1gi: {
-    fecha: "11-11-2023",
+    fecha: "10-11-2023",
     activate: true,
     admin: true,
-alias: nelm
+alias: "nelm"
 
     },
 
     A3m24332348: {
-    fecha: "11-22-2023",
+    fecha: "10-22-2023",
     activate: true,
     admin: true,
-        alias: querida
+        alias: "querida"
     },
 
     A5m3644h4kq: {
-    fecha: "11-22-2023",
+    fecha: "10-22-2023",
     activate: true,
     admin: true,
-        alias: dante
+        alias: "dante"
     },
 
     A7o4866468g: {
     fecha: "10-22-2023",
     activate: true,
     admin: true,
-        alias: primo
+        alias: "primo"
     },
 
     Aa05a87j7o6: {
     fecha: "10-22-2023",
     activate: false,
     admin: false,
-        alias: ident
+        alias: "ident"
     },
+    
+    A261211c1fc: {
+fecha: "11-22-2023",
+activate: false,
+admin: false,
+alias: "oreo0
+},
+
+A3m2422o32o: {
+fecha: "11-22-2023",
+activate: false,
+admin: false,
+alias: "oreo1
+},
+
+A5m364484h8: {
+fecha: "11-22-2023",
+activate: false,
+admin: false,
+alias: "oreo2
+},
+
+A7o4855k64k: {
+fecha: "11-22-2023",
+activate: false,
+admin: false,
+alias: "oreo3
+},
+
+Aa05a7747j4: {
+fecha: "11-22-2023",
+activate: false,
+admin: false,
+alias: "oreo4
+},
+
+Ac66c88g96g: {
+fecha: "11-22-2023",
+activate: false,
+admin: false,
+alias: "oreo5
+},
+
+Aee7eaa0al0: {
+fecha: "11-22-2023",
+activate: false,
+admin: false,
+alias: "oreo6
+},
+
+Ago8gbbcc8c: {
+fecha: "11-22-2023",
+activate: false,
+admin: false,
+alias: "oreo7
+},
+
+Aj89iccodno: {
+fecha: "11-22-2023",
+activate: false,
+admin: false,
+alias: "oreo8
+},
+
+Almakee8fa8: {
+fecha: "11-22-2023",
+activate: false,
+admin: false,
+alias: "oreo9
+},
+
 }
+
